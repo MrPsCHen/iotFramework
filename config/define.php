a240dfaffa588aef01713b6485e43ff333a25600
chenpeisen <cps_1993@126.com>
2021-09-03 13:34:38 +0800

    #0.1v

---
+++ config/define.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 站点路径
+ */
+define('PATH_ROOT'  , dirname(getcwd()).DIRECTORY_SEPARATOR);
+
+/**
+ * 公共文件路径
+ */
+define('PATH_PUBLIC', PATH_ROOT.'public'.DIRECTORY_SEPARATOR);
+
+/**
+ * 应用路径
+ */
+define('PATH_APP'   , PATH_ROOT.'app'.DIRECTORY_SEPARATOR);
+
+/**
+ * 配置路径
+ */
+define('PATH_CONFIG', PATH_ROOT.'config'.DIRECTORY_SEPARATOR);
+
+
+
+
+
